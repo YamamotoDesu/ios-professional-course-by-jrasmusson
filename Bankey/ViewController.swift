235c3a5e7c51e306881901e6a154fdceb5897058
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-01-10 09:34:49 +0900

    Initial Commit

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by 山本響 on 2022/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
